81eac0dd4fbb4a81b7c8c7567d5d45cb14690d53
gabrielastyrkowicz-cell <>
2026-01-12 20:20:50 +0000

    String Manipulation

---
+++ variables_and_types/string_manipulation/main.py
@@ -0,0 +1,10 @@
+grocery_items = "milk cheese bread apples oranges chicken"
+
+# Concatenating strings
+part1 = "Straw"
+part2 = "berry"
+new_word = part1 + part2  # "Strawberry"
+print("Concatenated word:", new_word)
+
+# If you want to separate the words with a space, you need to add " " between the two parts
+print("Word with spaces:", part1 + " " + part2)  # "Straw berry"
